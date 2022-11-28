490c3158ef7837dfd14cbf8d7272767e317fed95
Dylan Legendre <dylanlegendre09@gmail.com>
2022-11-27 22:38:23 -0600

    adding views

--- src/server/app.server.ts
+++ src/server/app.server.ts
@@ -5,8 +5,18 @@ export class AppServer {
   public static async start(): Promise<void> {
     const app = express();
 
+    // View Engine
+    app.set("view engine", "ejs");
+    app.set("views", "dist/views");
+
+    // Health Check
     app.get("/health", (_req, res, _next) => {
       res.json({ status: "OK" });
+    });
+
+    // Index Test
+    app.get("/", (_req, res, _next) => {
+      res.render("index", { title: "Hey", message: "Hello there!" });
     });
 
     const server = app.listen(8192);
